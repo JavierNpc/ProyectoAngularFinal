3d07e74a98542d7f69334690af1a10d450de2304
JavierNpc <javiermacedacastro@gmail.com>
2025-02-11 19:44:11 +0100

    mod

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -43,8 +43,8 @@ export default {
     'crearpersonaje/habilidades/index.html': {size: 3256, hash: '19a309df0357be866ec714d450c12c605ea342a3df5a4aa05697f209f68a2e1e', text: () => import('./assets-chunks/crearpersonaje_habilidades_index_html.mjs').then(m => m.default)},
     'crearpersonaje/nombre/index.html': {size: 3256, hash: '19a309df0357be866ec714d450c12c605ea342a3df5a4aa05697f209f68a2e1e', text: () => import('./assets-chunks/crearpersonaje_nombre_index_html.mjs').then(m => m.default)},
     'crearpersonaje/index.html': {size: 7208, hash: '9d315f159e1da81a44641792bde8549836b98a59d5ca6245c8554226ecb9233a', text: () => import('./assets-chunks/crearpersonaje_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 2966, hash: 'a17ee647d2efebd3ae5e289ddf6b5e5916847121e338bf9d3618e81008ac3380', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'crearpersonaje/estadisticas/index.html': {size: 3256, hash: 'b38b24c5e32bd5480b427e682c78b3ac9a47ff880fc3eff1f58f6022c0795b38', text: () => import('./assets-chunks/crearpersonaje_estadisticas_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 2966, hash: 'a17ee647d2efebd3ae5e289ddf6b5e5916847121e338bf9d3618e81008ac3380', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-SYYH6SQ4.css': {size: 212, hash: 'QEZ6RUlRMD0', text: () => import('./assets-chunks/styles-SYYH6SQ4_css.mjs').then(m => m.default)}
   },
 };
